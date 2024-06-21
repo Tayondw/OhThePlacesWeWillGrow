7c9ba1bab3097061dd591b449d98cce9a0a2cf09
Tayon <tayondw@gmail.com>
2024-06-20 20:14:26 -0400

    Update routes, model, and migration files for firstName and lastName attribues -> all routes passed

--- backend/db/migrations/20240620195502-create-user.js
+++ backend/db/migrations/20240620195502-create-user.js
@@ -17,6 +17,12 @@ module.exports = {
 					primaryKey: true,
 					type: Sequelize.INTEGER,
 				},
+				firstName: {
+					type: Sequelize.STRING,
+				},
+				lastName: {
+					type: Sequelize.STRING,
+				},
 				username: {
 					type: Sequelize.STRING(30),
 					allowNull: false,
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -43,6 +43,8 @@ router.post("/", validateLogin, async (req, res, next) => {
 
 	const safeUser = {
 		id: user.id,
+		firstName: user.firstName,
+		lastName: user.lastName,
 		email: user.email,
 		username: user.username,
 	};
@@ -66,6 +68,8 @@ router.get("/", (req, res) => {
 	if (user) {
 		const safeUser = {
 			id: user.id,
+			firstName: user.firstName,
+			lastName: user.lastName,
 			email: user.email,
 			username: user.username,
 		};
