56d499504e8bee05f5862ef38f38ad9c4eec59b0
Tayon <tayondw@gmail.com>
2024-06-24 20:02:12 -0400

    added seeders to groups, venues, events

--- backend/db/seeders/20240624213006-groupTest.js
+++ backend/db/seeders/20240624213006-groupTest.js
@@ -20,7 +20,91 @@ module.exports = {
 		 *   isBetaMember: false
 		 * }], {});
 		 */
-		await Group.bulkCreate([{}], { validate: true });
+		await Group.bulkCreate(
+			[
+				{
+					organizerId: 1,
+					name: "Group 1",
+					about:
+						"This is to test the FIRST test group.        11111111111111111111111111111111111111111111",
+					type: "Online",
+					private: true,
+					city: "Atlanta",
+					state: "GA",
+				},
+				{
+					organizerId: 2,
+					name: "Group 2",
+					about:
+						"This is to test the SECOND test group.        22222222222222222222222222222222222222222222",
+					type: "In person",
+					private: false,
+					city: "San Francisco",
+					state: "CA",
+				},
+				{
+					organizerId: 3,
+					name: "Group 3",
+					about:
+						"This is to test the THIRD test group.        33333333333333333333333333333333333333333333",
+					type: "Online",
+					private: true,
+					city: "Seattle",
+					state: "WA",
+				},
+				{
+					organizerId: 4,
+					name: "Group 4",
+					about:
+						"This is to test the FOURTH test group.        444444444444444444444444444444444444444444444",
+					type: "In person",
+					private: false,
+					city: "Philadelphia",
+					state: "PA",
+				},
+				{
+					organizerId: 4,
+					name: "Group 5",
+					about:
+						"This is to test the FIFTH test group.        555555555555555555555555555555555555555555555",
+					type: "Online",
+					private: true,
+					city: "Alexandria",
+					state: "VA",
+				},
+				{
+					organizerId: 3,
+					name: "Group 6",
+					about:
+						"This is to test the SIXTH test group.        666666666666666666666666666666666666666666666",
+					type: "In person",
+					private: true,
+					city: "Baton Rouge",
+					state: "LA",
+				},
+				{
+					organizerId: 2,
+					name: "Group 7",
+					about:
+						"This is to test the SEVENTH test group.        777777777777777777777777777777777777777777777",
+					type: "In person",
+					private: false,
+					city: "Boston",
+					state: "MA",
+				},
+				{
+					organizerId: 1,
+					name: "Group 8",
+					about:
+						"This is to test the EIGHTH test group.        88888888888888888888888888888888888888888888",
+					type: "Online",
+					private: false,
+					city: "Des Moines",
+					state: "IA",
+				},
+			],
+			{ validate: true }
+		);
 	},
 
 	async down(queryInterface, Sequelize) {
--- backend/db/seeders/20240624213018-venueTest.js
+++ backend/db/seeders/20240624213018-venueTest.js
@@ -20,7 +20,75 @@ module.exports = {
 		 *   isBetaMember: false
 		 * }], {});
 		 */
-		await Venue.bulkCreate([{}], { validate: true });
+		await Venue.bulkCreate(
+			[
+				{
+					groupId: 1,
+					address: "206 Washington Street",
+					city: "Atlanta",
+					state: "GA",
+					lat: 33.7490,
+					lng: -84.3881,
+				},
+				{
+					groupId: 2,
+					address: "1315 10th Street",
+					city: "San Francisco",
+					state: "CA",
+					lat: 38.5766,
+					lng: -121.4936,
+				},
+				{
+					groupId: 3,
+					address: "416 Sid Snyder Ave SW",
+					city: "Seattle",
+					state: "WA",
+					lat: 47.0357,
+					lng: -122.9048,
+				},
+				{
+					groupId: 4,
+					address: "501 N 3rd Street",
+					city: "Philadelphia",
+					state: "PA",
+					lat: 40.2643,
+					lng: -76.884,
+				},
+				{
+					groupId: 5,
+					address: "206 Washington Street",
+					city: "Alexandria",
+					state: "VA",
+					lat: 37.9268,
+					lng: -78.0249,
+				},
+				{
+					groupId: 6,
+					address: "900 North 3rd Street",
+					city: "Baton Rouge",
+					state: "LA",
+					lat: 30.2725,
+					lng: -91.1114,
+				},
+				{
+					groupId: 7,
+					address: "24 Beacon Street",
+					city: "Boston",
+					state: "MA",
+					lat: 42.3588,
+					lng: -71.0638,
+				},
+				{
+					groupId: 8,
+					address: "1007 E Grand Avenue",
+					city: "Des Moines",
+					state: "IA",
+					lat: 41.5911,
+					lng: -93.6038,
+				},
+			],
+			{ validate: true }
+		);
 	},
 
 	async down(queryInterface, Sequelize) {
