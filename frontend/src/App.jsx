cdcc45c69b5207c6eaace7cfb2679c2b49c9a248
Tayon <tayondw@gmail.com>
2024-07-12 18:42:13 -0400

    check signup modal passed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
 import Navigation from "./components/Navigation";
 import * as sessionActions from "./store/session";
 
@@ -30,10 +29,6 @@ const router = createBrowserRouter([
 			{
 				path: "/",
 				element: <h1>Welcome!</h1>,
-			},
-			{
-				path: "/signup",
-				element: <SignupFormPage />,
 			},
 		],
 	},
--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -3,28 +3,37 @@ import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector((state) => state.session.user);
 
-	const sessionLinks = sessionUser ? (
-		<li>
-			<ProfileButton user={sessionUser} />
-		</li>
-	) : (
-		<>
+	let sessionLinks;
+	if (sessionUser) {
+		sessionLinks = (
 			<li>
-				<OpenModalButton
-					buttonText="Log In"
-					modalComponent={<LoginFormModal />}
-				/>
+				<ProfileButton user={sessionUser} />
 			</li>
-			<li>
-				<NavLink to="/signup">Sign Up</NavLink>
-			</li>
-		</>
-	);
+		);
+	} else {
+		sessionLinks = (
+			<>
+				<li>
+					<OpenModalButton
+						buttonText="Log In"
+						modalComponent={<LoginFormModal />}
+					/>
+				</li>
+				<li>
+					<OpenModalButton
+						buttonText="Sign Up"
+						modalComponent={<SignupFormModal />}
+					/>
+				</li>
+			</>
+		);
+	}
 
 	return (
 		<ul>
