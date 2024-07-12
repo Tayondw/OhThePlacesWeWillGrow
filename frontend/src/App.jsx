7be561b4de49eb69591afaf7d444a76208795f2e
Tayon <tayondw@gmail.com>
2024-07-12 15:12:24 -0400

    complete phase 1 of authme: login user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,35 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
+
+const Layout = () => {
+	const dispatch = useDispatch();
+	const [isLoaded, setIsLoaded] = useState(false);
+
+	useEffect(() => {
+		dispatch(sessionActions.restoreUser()).then(() => {
+			setIsLoaded(true);
+		});
+	}, [dispatch]);
+
+	return <>{isLoaded && <Outlet />}</>;
+};
 
 const router = createBrowserRouter([
 	{
-		path: "/",
-		element: <h1>Welcome!</h1>,
-	},
-	{
-		path: "/login",
-		element: <LoginFormPage />,
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <h1>Welcome!</h1>,
+			},
+			{
+				path: "/login",
+				element: <LoginFormPage />,
+			},
+		],
 	},
 ]);
 
