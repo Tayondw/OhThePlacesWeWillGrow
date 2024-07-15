c7c4a38424dd0602499d7b642d5c8d401c15fa66
Tayon <tayondw@gmail.com>
2024-07-15 19:32:49 -0400

    fully complete HomePage component, work on groups next

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import { Link, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import "./HomePage.css"
+import "./HomePage.css";
 import collage1 from "../../assets/meetup-1.png";
 import collage2 from "../../assets/event-growth.png";
 import collage3 from "../../assets/meetup-4.png";
@@ -8,6 +8,8 @@ import collage4 from "../../assets/meetup-6.png";
 import allGroups from "../../assets/allGroups.png";
 import allEvents from "../../assets/allEvents.png";
 import newGroup from "../../assets/newGroup.png";
+import OpenModalButton from "../OpenModalButton";
+import SignupFormModal from "../SignupFormModal";
 
 const HomePage = () => {
 	const sessionUser = useSelector((state) => state.session.user);
@@ -18,8 +20,8 @@ const HomePage = () => {
 				<div className="intro">
 					<div className="intro-text">
 						<h1>
-							The cultivate platform — Where the places you go contain the places
-							in which you&apos;ll grow
+							The cultivate platform — Where the places you go contain the
+							places in which you&apos;ll grow
 						</h1>
 					</div>
 					<div className="intro-desc">
@@ -36,12 +38,12 @@ const HomePage = () => {
 						</p>
 					</div>
 				</div>
-                        <div className="img-holder">
-                              <img src={collage1} alt="meetup collage" />
-                              <img src={collage2} alt="meetup collage" />
-                              <img src={collage3} alt="meetup collage" />
-                              <img src={collage4} alt="meetup collage" />
-                        </div>
+				<div className="img-holder">
+					<img src={collage1} alt="meetup collage" />
+					<img src={collage2} alt="meetup collage" />
+					<img src={collage3} alt="meetup collage" />
+					<img src={collage4} alt="meetup collage" />
+				</div>
 			</div>
 			<div id="section-2">
 				<div className="meetup-text">
@@ -60,23 +62,63 @@ const HomePage = () => {
 			<div id="section-3">
 				<div className="meetup-links">
 					<img className="meetup-img" src={allGroups} alt="all-groups" />
-					<Link to="/groups">See all groups</Link>
-					<div className="link-desc"></div>
+					<Link className="nav-link" to="/groups">
+						See all groups
+					</Link>
+					<div className="link-desc">
+						<p>
+							The cultivate platform—Where the places you go contain the places
+							in which you will grow
+						</p>
+					</div>
 				</div>
 				<div className="meetup-links">
 					<img src={allEvents} alt="all-events" />
-					<Link to="/events">Find an event</Link>
-					<div className="link-desc"></div>
+					<Link className="nav-link" to="/events">
+						Find an event
+					</Link>
+					<div className="link-desc">
+						<p>
+							The cultivate platform—Where the places you go contain the places
+							in which you will grow
+						</p>
+					</div>
 				</div>
 				<div className="meetup-links">
 					<img src={newGroup} alt="new-group" />
-					<Link to="/groups">Start a new group</Link>
-					<div className="link-desc"></div>
+					{sessionUser ? (
+						<Link className="nav-link" to="/groups/create">
+							Start a new group
+						</Link>
+					) : (
+						<Link className="disabled" to="/groups/create">
+							Start a new group
+						</Link>
+					)}
+
+					<div className="link-desc">
+						<p>
+							The cultivate platform—Where the places you go contain the places
+							in which you will grow
+						</p>
+					</div>
 				</div>
 			</div>
 			<div id="section-4">
 				{sessionUser ? null : (
-					<button className="meetup-join">Join Meetup</button>
+					<OpenModalButton
+						buttonText="Join Us"
+						style={{
+							background: `linear-gradient(45deg, #ff5e5e, #135c51)`,
+							border: 0,
+							width: `250px`,
+							cursor: `pointer`,
+							borderRadius: `40px`,
+							padding: `12px 25px`,
+							fontSize: `1em`,
+						}}
+						modalComponent={<SignupFormModal />}
+					/>
 				)}
 			</div>
 			<Outlet />
