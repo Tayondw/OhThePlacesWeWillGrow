ab1115e5549c6fac36f0e44ab29bf77b46fb7238
Tayon <tayondw@gmail.com>
2024-07-15 14:27:53 -0400

    have home page laid out, need to add css and connect groups and events page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,15 @@ import { useDispatch } from "react-redux";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation-bonus";
 import * as sessionActions from "./store/session";
+import HomePage from "./components/HomePage";
 // import { useLoaderData } from "react-router-dom";
 // import { loginAction } from "./components/LoginFormModal/loginAction";
 // import { loginLoader } from "./components/LoginFormModal/loginAction";
 
 const Layout = () => {
-      const dispatch = useDispatch();
       // let user = useLoaderData();
       // console.log("checking for user", user);
+      const dispatch = useDispatch();
 	const [isLoaded, setIsLoaded] = useState(false);
 
 	useEffect(() => {
@@ -35,10 +36,7 @@ const router = createBrowserRouter([
 		children: [
 			{
 				path: "/",
-                        element: <>
-                              <h1>Growthup</h1>
-                              <p>The cultivate platform—Where the places you go contain the places in which you will grow</p>
-                        </>,
+                        element: <HomePage />,
                         
 			},
 		],
---
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,87 @@
+import { Link, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./HomePage.css"
+import collage1 from "../../assets/meetup-1.png";
+import collage2 from "../../assets/event-growth.png";
+import collage3 from "../../assets/meetup-4.png";
+import collage4 from "../../assets/meetup-6.png";
+import allGroups from "../../assets/allGroups.png";
+import allEvents from "../../assets/allEvents.png";
+import newGroup from "../../assets/newGroup.png";
+
+const HomePage = () => {
+	const sessionUser = useSelector((state) => state.session.user);
+
+	return (
+		<div id="all-four">
+			<div id="section-1">
+				<div className="intro">
+					<div className="intro-text">
+						<h1>
+							The cultivate platform — Where the places you go contain the places
+							in which you will grow
+						</h1>
+					</div>
+					<div className="intro-desc">
+						<p>
+							The cultivate platform—Where the places you go contain the places
+							in which you will growThe cultivate platform—Where the places you
+							go contain the places in which you will growThe cultivate
+							platform—Where the places you go contain the places in which you
+							will growThe cultivate platform—Where the places you go contain
+							the places in which you will growThe cultivate platform—Where the
+							places you go contain the places in which you will growThe
+							cultivate platform—Where the places you go contain the places in
+							which you will grow
+						</p>
+					</div>
+				</div>
+                        <div className="img-holder">
+                              <img src={collage1} alt="meetup collage" />
+                              <img src={collage2} alt="meetup collage" />
+                              <img src={collage3} alt="meetup collage" />
+                              <img src={collage4} alt="meetup collage" />
+                        </div>
+			</div>
+			<div id="section-2">
+				<div className="meetup-text">
+					<h2>Where We Will Grow</h2>
+				</div>
+				<div className="meetup-desc">
+					<p>
+						The cultivate platform—Where the places you go contain the places in
+						which you will growThe cultivate platform—Where the places you go
+						contain the places in which you will growThe cultivate
+						platform—Where the places you go contain the places in which you
+						will grow
+					</p>
+				</div>
+			</div>
+			<div id="section-3">
+				<div className="meetup-links">
+					<img src={allGroups} alt="all-groups" />
+					<Link to="/groups">See all groups</Link>
+					<div className="link-desc"></div>
+				</div>
+				<div className="meetup-links">
+					<img src={allEvents} alt="all-events" />
+					<Link to="/events">Find an event</Link>
+					<div className="link-desc"></div>
+				</div>
+				<div className="meetup-links">
+					<img src={newGroup} alt="new-group" />
+					<Link to="/groups">Start a new group</Link>
+					<div className="link-desc"></div>
+				</div>
+			</div>
+			<div id="section-4">
+				{sessionUser ? null : (
+					<button className="meetup-join">Join Meetup</button>
+				)}
+			</div>
+			<Outlet />
+		</div>
+	);
+};
+
+export default HomePage;
